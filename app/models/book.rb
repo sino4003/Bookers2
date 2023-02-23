5f0edc829f000a18b014ec6b066e677c16a5f1b1
EC2 Default User <ec2-user@ip-172-31-3-217.ap-northeast-1.compute.internal>
2023-02-23 13:16:27 +0000

    Bookers2

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,14 +4,5 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200}
 
-  has_one_attached :image
-
-  def get_image(width, height)
-    unless image.attached?
-      file_path = Rails.root.join("app/fakeimage bookers2/no_image.jpg")
-      image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpg")
-    end
-    image.variant(resize_to_limit: [width, height])
-  end
 
 end
